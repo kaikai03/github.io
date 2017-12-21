1fc80eb17ce73b328b8637244699382ec9f38af3
kai_kai03 <kai_kai03@hotmail.com>
2017-12-21 14:44:40 +0000

    remove kotoba

--- theme/js/damooctr.js
+++ theme/js/damooctr.js
@@ -1,9 +1,11 @@
 var hjs=document.createElement('script');
 hjs.setAttribute('id','blabla');
 document.body.appendChild(hjs);
+
+
 var inPage = true;
-var http_protocol = ('https:' == document.location.protocol) ? ' https://api.hitokoto.us:214' : ' http://api.hitokoto.us'
-var kotoba = http_protocol+'/rand?encode=jsc&fun=blabla'
+var http_protocol = ('https:' == document.location.protocol) ? ' https://sslapi.hitokoto.cn' : ' http://api.hitokoto.cn'
+var kotoba = http_protocol+'/?encode=json'
 
 function leavePage(){
             inPage = false;
@@ -14,6 +16,7 @@ function comebackPage(){
             inPage = true;
             console.log('comebackPage ');
         }
+        
         
 function dududu(){
     setTimeout(function(){
@@ -29,11 +32,23 @@ function dududu(){
 }
 
 window.onload=function(){ 
+            document.body.removeChild(document.getElementById('blabla')); 
+            var hjs=document.createElement('script');
+            hjs.setAttribute('id','blabla');
+            hjs.setAttribute('src',kotoba);
+            document.body.appendChild(hjs);
+            
+            var hjs2=document.getElementById('blabla')
+            console.log(hjs2);
+            console.log(hjs2.src);
+            
 	damoo.emit({ text: '欢迎光临----右舷弹幕启动', color: "#FFF", shadow: { color: "#f49" }});
-	dududu()
+	damoo.emit({ text: '由于资源问题弹幕话痨模式已关闭', color: "#FFF", shadow: { color: "#f49" }});
+    ukagaka.insert_kotoba('由于资源问题,弹幕话痨模式已关闭')
+    //dududu()
 } 
 
 function blabla(bla){
-    damoo.emit({ text: bla["author"]+"："+bla["hitokoto"], color: "#FFF", shadow: { color: "#f49" }});
+    damoo.emit({ text: bla["hitokoto"]+" by "+bla["from"], color: "#FFF", shadow: { color: "#f49" }});
     ukagaka.insert_kotoba(bla["hitokoto"])
 }
